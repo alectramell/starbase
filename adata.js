8e4313b93c7c401d15b2f87ea1b582819cfd1fed
alectramell <alectramell@gmail.com>
2017-05-02 00:17:03 -0700

    starbase

--- adata.js
+++
@@ -1,7 +0,0 @@
-function aCode() {
-	
-	var aCodeString = "686f6e6f72";
-	alert(aCodeString);
-}
-
-
---
+++ cdata.js
@@ -0,0 +1,19 @@
+function aCode() {
+	
+	var aCodeString = "1234";
+	alert(aCodeString);
+}
+
+function pCode() {
+	
+	var pCodeString = "1234";
+	alert(pCodeString);
+}
+
+function dCode() {
+	
+	var dCodeString = "1234";
+	alert(dCodeString);
+}
+
+
