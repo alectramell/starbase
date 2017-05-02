4013dc81d15564ccce30ef2df853ec5efcf8adc7
alectramell <alectramell@gmail.com>
2017-05-02 02:24:50 -0700

    starbase

--- cdata.js
+++ cdata.js
@@ -2,21 +2,21 @@ function aCode() {
 	
 	var aCodeString = "00000";
 	alert(aCodeString);
-	parent.location = "loadA.html";
+	// parent.location = "loadA.html";
 }
 
 function pCode() {
 	
 	var pCodeString = "000000";
 	alert(pCodeString);
-	parent.location = "loadP.html";
+	// parent.location = "loadP.html";
 }
 
 function dCode() {
 	
 	var dCodeString = "00000";
 	alert(dCodeString);
-	parent.location = "loadD.html";
+	// parent.location = "loadD.html";
 }
 
 
