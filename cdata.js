413a225aba6a2a37ae54f19ad9433b4b306abfdd
alectramell <alectramell@gmail.com>
2017-05-02 03:57:57 -0700

    starbase

--- cdata.js
+++ cdata.js
@@ -1,28 +1,22 @@
 function aCode() {
 	
 	var aCodeString = "0000";
-	var NewCode = window.prompt("Enter Apollonian Mission Code to Begin..","0000");
-	// alert(aCodeString);
-	// parent.location = "loadA.html";
-	// parent.location = "https://rawgit.com/alectramell/starbase/master/boxes/A/"+NewCode+".js";
+	var aCount = "0";
+	alert("..The Apollonian Data Archives..");
 }
 
 function pCode() {
 	
 	var pCodeString = "0000";
-	window.prompt("Enter Plantonian Mission Code to Begin..","0000");
-	// alert(pCodeString);
-	// parent.location = "loadP.html";
-	// parent.location = "https://rawgit.com/alectramell/starbase/master/boxes/P/"+NewCode+".js";
+	var pCount = "0";
+	alert("..The Plantonian Data Archives..");
 }
 
 function dCode() {
 	
 	var dCodeString = "0000";
-	window.prompt("Enter Dionysian Mission Code to Begin..","0000");
-	// alert(dCodeString);
-	// parent.location = "loadD.html";
-	// parent.location = "https://rawgit.com/alectramell/starbase/master/boxes/D/"+NewCode+".js";
+	var dCount = "0";
+	alert("..The Dionysian Data Archives..");
 }
 
 
